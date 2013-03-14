6d941d3b76f1d7b18db4660babf83fd43b1a7233
Doug Yun <yun.douglas@gmail.com>
2013-03-14 00:15:29 -0400

    Reorganized and started fooling with minitest.

--- 3/3_alt.rb
+++
@@ -1,7 +0,0 @@
-# The prime factors of 13195 are 5, 7, 13 and 29.
-
-# What is the largest prime factor of the number 600851475143 ?
-
-require 'prime'
-
-p 600851475143.prime_division.flatten.max
--- ruby/1.rb
+++ ruby/1.rb
@@ -3,12 +3,30 @@
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = 0
+require 'minitest/autorun'
 
-1000.times do |x|
-  if x % 3 == 0 || x % 5 ==0
-    sum += x
+class TargetNumber
+  def add_multiples(numbers)
+    sum = 0
+    numbers.times do |x|
+      if x % 3 == 0 || x % 5 == 0
+        sum += x
+      end
+    end
+    sum
   end
 end
 
-puts sum
+### Test ###
+
+describe TargetNumber do
+  it 'is given the number 10' do
+    target_number = TargetNumber.new
+    target_number.add_multiples(10).must_equal 23
+  end
+end
+
+### Trial ###
+
+test_number = TargetNumber.new
+p test_number.add_multiples(1000)
